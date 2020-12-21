2b54de9c607fdd185b712cc8ba89f1756919512f
basura-persistent <rohitbasu1997@gmail.com>
2020-12-20 19:08:30 -0800

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Rohit Basu on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
